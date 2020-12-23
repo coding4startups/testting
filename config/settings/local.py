168ce1012c408a2a91442dcd94b081a3aa3f43ea
datasciencedotuniversity <datasciencedotuniversity@gmail.com>
2020-12-23 10:30:26 +0100

    This is local host added

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="efkfcnmuWlQ72DNzWHubJ253DXsWrtz8KnXgeIRnarjogxSqsnAq8W66gg0tTF6Z",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1","54.169.16.189"]
 
 # CACHES
 # ------------------------------------------------------------------------------
